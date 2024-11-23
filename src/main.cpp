ca2d7f739115505a001296f67c792bcf4b1db5ab
yarikefremov <161132970+yarikefremov@users.noreply.github.com>
2024-11-23 17:32:04 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "../include/gui.h"
 #include "../include/tcpclient.h"
+#include "../include/inetgui.h"
 
 int main() {
-    TCPClient tcp;
-    //GUI gui;
+    NetworkGUIInterface inetgui;
+    TCPClient tcp(&inetgui);
+    //GUI gui(&inetgui);
     //std::thread netThread(netThreadFunc, gui.netclient);
     //gui.init();
     return 0;
