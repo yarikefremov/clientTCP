14917f30424d205c62caeec1bd83f617038c82fc
yarikefremov <161132970+yarikefremov@users.noreply.github.com>
2024-11-23 17:33:38 +0300

    Update gui.h

--- include/gui.h
+++ include/gui.h
@@ -5,7 +5,7 @@
 
 class GUI{
 public:
-    GUI();
+    GUI(NetworkGUIInterface *inetgui);
     void init();
 private:
 
@@ -14,7 +14,7 @@ private:
     bool isRegisterMode = false;
     bool isChatMode = false;
     bool isAddingUsername = false;
-    NetworkGUIInterface inetgui;
+    NetworkGUIInterface* inetgui;
     sf::String nameInput, passwordInput, currentMessage;
     std::vector<sf::Text> chatLog;
     std::vector<std::string> users{"123", "key77", "gregor12", "michael1907"};
